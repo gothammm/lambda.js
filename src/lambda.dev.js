979d9bbb11c901a6220d64fd009740f50d4c852b
peek4y <gautham2r@gmail.com>
2014-05-14 11:00:52 +0000

    added where functionality

--- src/lambda.dev.js
+++ src/lambda.dev.js
@@ -13,10 +13,47 @@
     },
     isOfTypeAttached: protoAttach
   };
+  lambda._util = {
+    mapKeyValue: function(obj) {
+      var keyValue = [];
+      if(!obj) lambda.exception.throwEx("obj is undefined");
+      if(typeof(obj) != "object") lambda.exception.throwEx("obj must be of type object");
+      
+      for(var key in obj) {
+        keyValue.push({ "key" : key, "value": obj[key] });
+      }
+      return keyValue;
+    }
+  };
   lambda.exception = {
     throwEx: function(e) {
       console.log(e);
       throw e;
+    }
+  };
+  lambda._query = {
+    or: false,
+    and: false,
+    expression: [],
+    type: {
+      name: "query",
+      hex: "7175657279"
+    },
+    isOfTypeQuery: function(obj) {
+      if(!obj) {
+        return false;
+      }
+      if(!obj.type) {
+        return false;
+      }
+      if(obj.type) {
+        var typeObj = obj.type;
+        if(!typeObj) return false;
+        if(typeof(typeObj) != 'object') return false;
+        if(!typeObj.name || !typeObj.hex) return false;
+        if(typeObj.name == "query" && typeObj.hex == "7175657279") return true;
+      }
+      return false;
     }
   };
   lambda._actions = {
@@ -75,15 +112,36 @@
       return arr;
       
     },
-    _where: function(query) {
-      
+    _where: function(arr, query) {
+      var me = this;
+      if(!arr) lambda.exception.throwEx("Array is undefined");
+      if(!arr.length && typeof(arr) == 'object') lambda.exception.throwEx("'arr' parameter must be of type Array");
+      if(!query) lambda.exception.throwEx("'query' is undefined");
+      if(typeof(query) != "object") lambda.exception.throwEx("'query' parameter must be of type 'object'");
+      var i = 0, j, arrLength = arr.length, result = [];
+      var isOfTypeQuery = lambda._query.isOfTypeQuery(query);
+      if(!isOfTypeQuery) {
+        var keyValue = me._first(lambda._util.mapKeyValue(query));
+        for(; i < arrLength; i++) {
+          var arrObj = arr[i];
+          if(arrObj) {
+            var key = keyValue.key;
+            var value = keyValue.value;
+            if(arrObj[key] && arrObj[key] == value) {
+              result.push(arrObj);
+            }
+          }
+        }
+      }
+      return result;
     },
     _getActions: function() {
       var me = this;
       return {
         select: me._select,
-        first: me._first
-      }
+        first: me._first,
+        where: me._where
+      };
     }
   };
   lambda._setActionsToArrayPrototype = {
@@ -94,6 +152,9 @@
       },
       getFirstAction: function() {
         return this.collection.first;
+      },
+      getWhereAction: function() {
+        return this.collection.where;
       }
     },
     set: function() {
@@ -106,14 +167,21 @@
       var actionsObj = this.actions;
       Array.prototype.select = function(props) {
         var me = this;
-        return actionsObj.getSelectAction(me, props);
+        return actionsObj.getSelectAction()(me, props);
       }
     },
     _setForArrayFirst: function() {
       var actionsObj = this.actions;
       Array.prototype.first = function() {
         var me = this;
-        return actionsObj.getFirstAction(me);
+        return actionsObj.getFirstAction()(me);
+      }
+    },
+    _setForArrayWhere: function() {
+      var actionsObj = this.actions;
+      Array.prototype.where = function(query) {
+        var me = this;
+        return actionsObj.getWhereAction()(query);
       }
     }
   };
@@ -122,4 +190,5 @@
   var actionsList = lambda._actions._getActions();
   lambda.select = actionsList.select;
   lambda.first = actionsList.first;
+  lambda.where = actionsList.where;
 })();
